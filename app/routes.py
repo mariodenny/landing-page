bdfbbe5a67458f7711fbd66bcf6ae8b5c417a1ce
mariodenny <dennym.channel@gmail.com>
2026-02-16 18:57:08 +0700

    setup for docker

---
+++ app/routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+main = Blueprint("main", __name__)
+
+@main.route("/")
+def home():
+    return render_template("index.html")
+
+@main.route("/leaderboard")
+def leaderboard():
+    return render_template("leaderboard.html")
+
+@main.route("/about")
+def about():
+    return render_template("about.html")
